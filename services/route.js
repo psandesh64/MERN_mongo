c1edd8a7d8d7e17b003389406a9c8ea018a90d6e
psandesh64 <psandesh64@gmail.com>
2024-01-12 22:10:05 +0545

    Error Handling

--- services/route.js
+++ services/route.js
@@ -5,6 +5,12 @@ router.get('/api/contacts',(request,response) => {
     Contact.find({}).then(result => response.json(result))
 })
 
+router.get('/api/contacts/:id',(request,response) => {
+    Contact.findById(request.params.id)
+    .then(result => result ? response.json(result) : 
+    response.status(404).json({error : 'contact not found'}))
+    .catch(error => response.status(500).json({error: 'Internal Server Error'}))
+})
 
 router.post('/api/contacts',(request,response) => {
     const body = request.body
